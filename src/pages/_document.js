bba00b5a534afeb83e44fd5267b5f9d30f34b7a2
Hassan1228 <126410471+Hassan1228@users.noreply.github.com>
2023-09-18 19:24:31 +0500

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -63,7 +63,7 @@ class MyDocument extends Document {
           <link
             href="https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
             rel="stylesheet"
-          />
+          /> 
           <link
             href="https://fonts.googleapis.com/css2?family=Kumbh+Sans:wght@300;400;600;700;800;900&display=swap"
             rel="stylesheet"
