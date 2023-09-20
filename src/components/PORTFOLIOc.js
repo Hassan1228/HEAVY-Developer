254e6314cba4f443bbce2fae30858f2653ef10f1
Hassan1228 <126410471+Hassan1228@users.noreply.github.com>
2023-09-20 22:22:32 +0500

    Update PORTFOLIOc.js

--- src/components/PORTFOLIOc.js
+++ src/components/PORTFOLIOc.js
@@ -101,13 +101,14 @@ function PORTFOLIOc() {
                     </div>
            
                     <div id="container">
-                 
+                  <PageBanner6 title="Portfolio" />
                         <section >
- <PageBanner6 title="Portfolio" />
+
+                    <MyComponent/>
                         </section>
                 
                         <section />
-   <MyComponent/>
+ 
                         <FooterSeven/>
                         <section />
                         <section />
